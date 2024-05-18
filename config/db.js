5a82adbf6e98737c920919321c33e4a28591a4f8
MUSTKEEM AHMAD <46074466+Mustkeem324@users.noreply.github.com>
2024-05-18 08:26:24 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI");
+const mongoURI = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
@@ -18,5 +18,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-const db = config.get("mongoURI"); 
