62dc2a32c8932ef1027694a99c553a2d67ccd610
MUSTKEEM AHMAD <46074466+Mustkeem324@users.noreply.github.com>
2024-05-18 08:15:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
-require("dotenv").config(); 
-
-const db = process.env.MONGO_URI; 
+const config = require("config");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
@@ -13,7 +11,7 @@ const connectDB = async () => {
 
     console.log("MongoDB connected");
   } catch (error) {
-    console.error("Something went wrong with the database connection", error);
+    console.error("Something went wrong with Database connection:", error.message);
     process.exit(1);
   }
 };
